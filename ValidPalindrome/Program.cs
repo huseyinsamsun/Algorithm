be474ee58b1e30a30eacc04763e89c8440a86ccf
huseyinsamsun <huseyinsamsun81@gmail.com>
2025-04-23 22:17:08 +0300

    ds

---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+IsPalindrome("A man, a plan, a canal: Panama");
+
+bool IsPalindrome(string s)
+{
+    if (string.IsNullOrEmpty(s))
+        return true;
+
+    StringBuilder stringBuilder = new StringBuilder();
+    char[] words = s.ToCharArray();
+
+    foreach (var item in words)
+    {
+        if (char.IsLetterOrDigit(item))
+            stringBuilder.Append(item.ToString().ToLower());
+    }
+
+    string finalWords = stringBuilder.ToString();
+    words = finalWords.ToCharArray();
+    Array.Reverse(words);
+    string reversed = new string(words);
+    if (finalWords == reversed)
+        return true;
+
+    return false;
+}
