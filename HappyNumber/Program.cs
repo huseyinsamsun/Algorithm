63d117de8e7cf14aba51425f6f3bc71bdbaf3d61
huseyinsamsun <huseyinsamsun81@gmail.com>
2025-04-24 21:17:31 +0300

    happy number :)

---
+++ HappyNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿IsHappy(19);
+
+bool IsHappy(int n)
+{
+    List<double> numbers = new List<double>();
+    double sum = 0;
+    var numberAr = n.ToString().ToCharArray();
+    for (int i = 0; i < numberAr.Length + 1; i++)
+    {
+        if (i < numberAr.Length)
+        {
+            sum += Math.Pow(char.GetNumericValue(numberAr[i]), 2);
+        }
+
+        if (i == numberAr.Length)
+        {
+            if (sum == 1)
+            {
+                return true;
+            }
+
+            if (numbers.Contains(sum))
+            {
+                return false;
+            }
+
+            numbers.Add(sum);
+            numberAr = sum.ToString().ToCharArray();
+            i = -1;
+            sum = 0;
+        }
+    }
+    return false;
+}
+
+;
