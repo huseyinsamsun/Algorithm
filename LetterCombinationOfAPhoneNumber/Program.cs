64e27caa72b251e2d49cc35ce90b6db07a3ab631
huseyinsamsun <huseyinsamsun81@gmail.com>
2025-04-28 21:39:44 +0300

    contiunes...

---
+++ LetterCombinationOfAPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿ IList<string> LetterCombinations(string digits)
+ {
+        
+     char[] digitArray = digits.ToCharArray();
+     double countOfDigits = 0;
+
+     for (int i = 0; i < digits.Length; i++)
+     {
+         countOfDigits *= char.GetNumericValue(digitArray[i]);
+
+     }
+
+     for (int i = 0; i < countOfDigits; i++)
+     {
+         
+     }
+
+     return new List<string>();
+ }
